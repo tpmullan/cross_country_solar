a33d5e5689e4d97328c2dd5a4abcda06b25f4137
Tom Mullaney <tpmullan@gmail.com>
2016-07-09 00:01:39 -0500

    Added ruby version to gemfile

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Shane Besong on 5/17/16.
+ */
+
+//function initialize() {
+//    var mapProp = {
+//        center:new google.maps.LatLng(0,0),
+//        zoom:5,
+//        mapTypeId:google.maps.MapTypeId.ROADMAP
+//    };
+//    var map = new google.maps.Map(document.getElementById("map"),mapProp);
+//}
+//google.maps.event.addDomListener(window, 'load', initialize);
